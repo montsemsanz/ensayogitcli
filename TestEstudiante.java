ed094ab05a8d7af93439fa95fa87969b0f72d3f2
Santi Lerga <slergacdam1@gmail.com>
2022-11-30 16:50:58 +0100

    Nuevo estudiante añadido

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -18,7 +18,8 @@ public class TestEstudiante
         e2.printEstudiante();
         Estudiante e3 = new Estudiante("Elisa", 26);
         e3.printEstudiante();
-        
+        Estudiante e4 = new Estudiante("Santi", 19);
+        e4.printEstudiante();
 
     }
 }
