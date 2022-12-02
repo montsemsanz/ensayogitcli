36c5e19e6495c62304bcc66f8b2f55317646528e
Jagoba Inda <jindaardam1@gmail.com>
2022-12-02 21:06:33 +0100

    Añadido alumno 4

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -18,6 +18,8 @@ public class TestEstudiante
         e2.printEstudiante();
         Estudiante e3 = new Estudiante("Maite", 26);
         e3.printEstudiante();
+        Estudiante e4 = new Estudiante("Jagoba", 22);
+        e4.printEstudiante();
         
 
     }
